ea9cab9e66f6ebe1ee6d73dbd0a3e67ee671b53d
Majid Husain <majidsmart7@gmail.com>
2025-02-27 16:38:55 +0530

    ml

--- App.js
+++ App.js
@@ -1,71 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AdBuses from "./components/Admin/BusRoute/AdBuses";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.chatContainer}>
-        <View style={styles.messageBubble}>
-          <Text style={styles.messageText}>Hello! majid husain
-             How are you?</Text>
-        </View>
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Type a message..." />
-        <TouchableOpacity style={styles.sendButton}>
-          <Text style={styles.sendButtonText}>Sendtoyouda</Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="adbuses">
+        <Stack.Screen name="adbuses" component={AdBuses} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f0f0f0',
-    justifyContent: 'flex-end',
-  },
-  chatContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: 'flex-end',
-  },
-  messageBubble: {
-    backgroundColor: '#007bff',
-    padding: 10,
-    borderRadius: 10,
-    maxWidth: '70%',
-    alignSelf: 'flex-start',
-    marginBottom: 10,
-  },
-  messageText: {
-    color: '#fff',
-    fontSize: 16,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    padding: 10,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
-  input: {
-    flex: 1,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    marginRight: 10,
-  },
-  sendButton: {
-    backgroundColor: '#007bff',
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    borderRadius: 8,
-  },
-  sendButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-});
